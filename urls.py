993c1e84b1c40e2efc2c29092dc0a3ffd2f9c96b
John Maxwell <astrofinch@astrofinch.com>
2010-07-13 15:17:09 -0700

    Make basic voting work...
    Replace djangoratings with git fork, etc.

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 
 urlpatterns = patterns('',
     (r'^lists/', include('my.lists.urls')),
-    # (r'^rate/', include('my.djangoratings.urls')),
+    (r'^rate/', include('my.djangoratings.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^$', lambda request: HttpResponseRedirect('/lists/')),
