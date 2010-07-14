bec1d2020c54d4007960d66ff4faee5aa7a32aa7
John Maxwell <astrofinch@astrofinch.com>
2010-07-14 11:25:58 -0700

    Make it so old star ratings are presented...
    Make it so star ratings are transmitted via AJAX.  Stop using generic
    views.  Etc.

--- lists/urls.py
+++ lists/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls.defaults import *
 from my.lists.models import List
 
-info_dict = {
-    'queryset': List.objects.all(),
-}
-
-urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
-    (r'^(?P<object_id>\d+)/$',
-        'django.views.generic.list_detail.object_detail', info_dict),
+urlpatterns = patterns('my.lists.views',
+    (r'^$', 'list'),
+    (r'^(?P<object_id>\d+)/$', 'detail'),
 )
--- lists/views.py
+++ lists/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.views.generic.simple import direct_to_template as template
+from my.lists.models import *
+
+def list(request):
+    return template(request, 'lists/list_list.html', {
+        'objects': List.objects.all(),
+        'request': request,
+    })
+
+def detail(request, object_id):
+    return template(request, 'lists/list_detail.html', {
+        'object': List.objects.get(id=object_id),
+        'request': request,
+    })
