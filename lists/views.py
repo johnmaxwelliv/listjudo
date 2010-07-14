0ef5adcfa16608493c245d6386da195a24c8d090
John Maxwell <astrofinch@astrofinch.com>
2010-07-14 16:28:21 -0700

    Make it so a list need not be chosen for entries

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,16 @@
 from django.views.generic.simple import direct_to_template as template
 from my.lists.models import *
 from django.forms import ModelForm
+from django.forms.widgets import HiddenInput
 from django.http import HttpResponse
 
 class EntryForm(ModelForm):
     class Meta:
         model = Entry
+        fields = ['title', 'description', 'list']
+        widgets = {
+            'list': HiddenInput(),
+        }
 
 def list(request):
     return template(request, 'lists/list_list.html', {
@@ -14,14 +19,18 @@ def list(request):
     })
 
 def detail(request, object_id):
+    object = List.objects.get(id=object_id)
+    empty_entry = Entry(list=object)
     return template(request, 'lists/list_detail.html', {
-        'object': List.objects.get(id=object_id),
+        'object': object,
         'request': request,
-        'form': EntryForm(),
+        'form': EntryForm(instance=empty_entry),
     })
 
 def add_entry(request, object_id):
     form = EntryForm(request.POST)
     if form.is_valid():
         form.save()
-    return HttpResponse("Entry saved.")
+        return HttpResponse("Entry saved.")
+    else:
+        return HttpResponse("Bad entry.")
