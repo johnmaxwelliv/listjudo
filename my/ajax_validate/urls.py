61e264bb7f58b0c51c0a1d449ab4ad645e0c5b07
John Maxwell <astrofinch@astrofinch.com>
2010-08-02 12:04:27 -0700

    Fix ajax validation for list creation form

--- my/ajax_validate/urls.py
+++ my/ajax_validate/urls.py
@@ -4,4 +4,5 @@ from my.lists.views import *
 urlpatterns = patterns('ajax_validation.views',
     # (r'^SOME/URL/$', 'ajax_validation.views.validate', {'form_class': ContactForm}, 'contact_form_validate')
     (r'^entry_form/$', 'validate', {'form_class': EntryForm}, 'entry_form_validate'),
+    (r'^list_form/$', 'validate', {'form_class': ListForm}, 'list_form_validate'),
 )
