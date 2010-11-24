459c3e4f3d3a8e73e0c33e8a0dd34d24450c3f20
John Maxwell <astrofinch@astrofinch.com>
2010-11-24 15:26:52 -0800

    Create a header with an embedded font

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ MEDIA_ROOT = SITE_ROOT.child('media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8888/media'
+MEDIA_URL = 'http://127.0.0.1:8888/media'
 
 DJANGO_STATIC = False
 DJANGO_STATIC_MEDIA_URL = '/media'
