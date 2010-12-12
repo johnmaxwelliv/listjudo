cf0cd9a7a2549dde68244c7b4da36903cf3c0add
John Maxwell <astrofinch@astrofinch.com>
2010-12-11 21:05:28 -0800

    Fix absolute path bug

--- settings.py
+++ settings.py
@@ -90,7 +90,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/john/li/templates/',
+    REPO_ROOT.child('templates'),
 )
 
 INSTALLED_APPS = (
